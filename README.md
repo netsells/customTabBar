4fd6e0096acf574edd522695dff7053c111185fa
Sam Jordan <sam@netsells.co.uk>
2011-02-12 23:18:42 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Custom Tab Bar
+==============
+
+Easily add a custom tabbar to your Appcelerator Titanium iPhone app!
+
+All available settings are shown in the example, defaults will be along shortly :)
+
+Example
+--------
+
+	// Create the tab group
+	var tabGroup = Titanium.UI.createTabGroup();
+
+	// Assign windows & tabs
+	// IMPORTANT:
+	//			'tabBarHidden: true' should be set on all windows
+	//			height should be set to 480 - customTabBar's height (change 480 to app screen height)
+	var win1 = Titanium.UI.createWindow({ title:'Tab 1', height: 440, tabBarHidden: true });
+	var tab1 = Titanium.UI.createTab({ window:win1 });
+	var win2 = Titanium.UI.createWindow({ title:'Tab 2', height: 440, tabBarHidden: true });
+	var tab2 = Titanium.UI.createTab({ window:win2 });
+	tabGroup.addTab(tab1);  
+	tabGroup.addTab(tab2);  
+
+	// open tab group
+	tabGroup.open();
+
+	// Here is the magic
+	Ti.include("customTabBar.js");
+
+	var ctb = new CustomTabBar({
+		imagePath: 'iphone/images/',
+		width: 80,
+		height: 40,
+		items: [
+			{ image: 'home.png', selected: 'home_over.png' },
+			{ image: 'cloud.png', selected: 'cloud_over.png' },
+			{ image: 'cart.png', selected: 'cart_over.png' },
+			{ image: 'settings.png', selected: 'settings_over.png' }
+		]
+	});
---
+++ customTabBar.js
@@ -0,0 +1,67 @@
+CustomTabBar = function(settings) {
+	var tabBarItems = [];
+	
+	var resetTabs = function() {
+		for(var i = 0; i < tabBarItems.length; i++) {
+			// Clear all the images to make sure only
+			// one is shown as selected
+			tabBarItems[i].image = null;
+		}
+	};
+	
+	var assignClick = function(tabItem) {
+		tabItem.addEventListener('click', function(e) {
+			// Just fetching the 'i' variable from the loop
+			var pos = e.source.pos;
+			
+			// Switch to the tab associated with the image pressed
+			tabGroup.tabs[pos].active = true;
+			
+			// Reset all the tab images
+			resetTabs();
+			
+			// Set the current tab as selected
+			tabBarItems[pos].image = settings.imagePath + settings.items[pos].selected;		
+		});
+	};
+	
+	// Create the container for our tab items
+	var customTabBar = Ti.UI.createWindow({
+		height: settings.height,
+		bottom: 0
+	});
+	
+	
+	for(var i = 0; i < settings.items.length; i++) {
+		// Go through each item and create an imageView
+		tabBarItems[i] = Titanium.UI.createImageView({
+			// background is the default image
+			backgroundImage: settings.imagePath + settings.items[i].image,
+			
+			// image is the selected image
+			image: settings.imagePath + settings.items[i].selected,
+			width: settings.width,
+			height: settings.height,
+			left: settings.width * i
+		});
+
+		// Pass the item number (used later for changing tabs)
+		tabBarItems[i].pos = i;
+		assignClick(tabBarItems[i]);
+
+		// Add to the container window
+		customTabBar.add(tabBarItems[i]);
+	}
+
+	// Display the container and it's items
+	customTabBar.open();
+
+	// Set the first item as current :)
+	resetTabs();
+	tabBarItems[0].image = settings.imagePath + settings.items[0].selected;
+	
+	return {
+		hide: function() { customTabBar.hide(); },
+		show: function() { customTabBar.show(); }
+	}
+};
